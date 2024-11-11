21375e1c72c1b444696ec427fe481e5f6ff1446e
Om Bhor <ombhor617@gmail.com>
2024-11-12 03:21:12 +0530

    featured section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import HeroSection from "@/components/HeroSection";
+import FeaturedCourses from "@/components/ui/FeaturedCourses";
 
 
 
@@ -6,6 +7,7 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection/>
+      <FeaturedCourses/>
     </main>
   )
 }
